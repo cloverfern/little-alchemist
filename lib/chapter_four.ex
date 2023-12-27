bf1a8c62b0da43313dca69d870e3def39a01539e
Fern <frlimaa@gmail.com>
2023-12-26 22:37:05 -0600

    minus

--- lib/chapter_four.ex
+++ lib/chapter_four.ex
@@ -5,6 +5,7 @@ defmodule ChapterFour do
   defp add1(n), do: n + 1
   defp sub1(n), do: n - 1
 
+  @spec plus(any(), number()) :: any()
   @doc """
   Adds two positive numbers together
   ## Examples
@@ -19,24 +20,46 @@ defmodule ChapterFour do
     plus(n1, sub1(n2))
     |> add1
   end
+  """
+  plus(1, 2)>
+    plus(1, 1)>
+      plus(1, 0)> 1
+    add1(1) > 2
+  add1(2) > 3
+  """
 
   @doc """
   Adds two positive numbers together. Tail call optimized
   ## Examples
-  iex> ChapterFour.plus2(46, 12)
+  iex> ChapterFour.plus_opt(46, 12)
   58
   """
-  def plus2(n1, n2) do
-    plus2_helper(n1, n2, 0)
+  def plus_opt(n1, n2) do
+    plus_opt_helper(n1, n2, 0)
   end
 
-  defp plus2_helper(n1, 0, acc) do
+  defp plus_opt_helper(n1, 0, acc) do
     n1 + acc
   end
 
-  defp plus2_helper(n1, n2, acc) do
-    plus2_helper(n1, sub1(n2), add1(acc))
+  defp plus_opt_helper(n1, n2, acc) do
+    plus_opt_helper(n1, sub1(n2), add1(acc))
   end
 
+  def minus(n1, 0) do
+    n1
+  end
+
+  @spec minus(any(), number()) :: any()
+  @doc """
+  Subtracts n2 from n1
+  ## Examples
+  iex> ChapterFour.minus(46, 12)
+  34
+  """
+  def minus(n1, n2) do
+    minus(n1, sub1(n2))
+    |> sub1
+  end
 
 end
