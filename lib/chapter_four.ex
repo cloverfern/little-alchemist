47df259319622d1bb0b4feb1568368088216033f
Fern <frlimaa@gmail.com>
2023-12-27 08:52:07 -0600

    handling negative numbers in plus, added tail call optimized minus

--- lib/chapter_four.ex
+++ lib/chapter_four.ex
@@ -5,29 +5,35 @@ defmodule ChapterFour do
   defp add1(n), do: n + 1
   defp sub1(n), do: n - 1
 
-  @spec plus(any(), number()) :: any()
+  @spec plus(number, number) :: number
   @doc """
-  Adds two positive numbers together
+  Adds two numbers together
   ## Examples
   iex> ChapterFour.plus(46, 12)
   58
+
+  iex > ChapterFour.plus(-1, -2)
+  -3
+
+  iex > ChapterFour.plus(-6, 2)
+  -4
   """
   def plus(n1, 0) do
     n1
+  end
+
+  def plus(n1, n2)
+  when n2 < 0 do
+    plus(n1, add1(n2))
+    |> add1
   end
 
   def plus(n1, n2) do
     plus(n1, sub1(n2))
     |> add1
   end
-  """
-  plus(1, 2)>
-    plus(1, 1)>
-      plus(1, 0)> 1
-    add1(1) > 2
-  add1(2) > 3
-  """
 
+  @spec plus_opt(number, number) :: number
   @doc """
   Adds two positive numbers together. Tail call optimized
   ## Examples
@@ -46,20 +52,39 @@ defmodule ChapterFour do
     plus_opt_helper(n1, sub1(n2), add1(acc))
   end
 
-  def minus(n1, 0) do
-    n1
-  end
-
-  @spec minus(any(), number()) :: any()
+  @spec minus(number, number) :: number
   @doc """
   Subtracts n2 from n1
   ## Examples
   iex> ChapterFour.minus(46, 12)
   34
   """
+  def minus(n1, 0) do
+    n1
+  end
+
   def minus(n1, n2) do
     minus(n1, sub1(n2))
     |> sub1
+  end
+
+  @spec minus_opt(number, number) :: number
+  @doc"""
+  Substracts n2 from n1. Tail call optimized
+  ## Examples
+  iex> ChapterFour.minus_opt(46, 12)
+  34
+  """
+  def minus_opt(n1, n2) do
+    minus_opt_helper(n1, n2, 0)
+  end
+
+  defp minus_opt_helper(n1, 0, acc) do
+    n1 - acc
+  end
+
+  defp minus_opt_helper(n1, n2, acc) do
+    minus_opt_helper(n1, sub1(n2), add1(acc))
   end
 
 end
